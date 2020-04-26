0879272185ad17038a7aa04fc53f526e990df758
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-04-26 10:51:47 +0900

    cosmetic changes to ReadSideMain

--- src/main/java/org/mvrck/training/app/ReadSideMain.java
+++ src/main/java/org/mvrck/training/app/ReadSideMain.java
@@ -9,14 +9,13 @@ import org.mvrck.training.config.*;
 import org.mvrck.training.dao.OrderDaoImpl;
 import org.mvrck.training.dao.TicketStockDaoImpl;
 import org.mvrck.training.entity.*;
-import org.seasar.doma.jdbc.tx.*;
 
 public class ReadSideMain {
   public static void main(String[] array) {
     var system = ActorSystem.create(Behaviors.empty(), "readside-guardian");
     var journal = PersistenceQuery.get(system).getReadJournalFor(JdbcReadJournal.class, JdbcReadJournal.Identifier());
 
-    TransactionManager transactionManager = AppConfig.singleton().getTransactionManager();
+    var transactionManager = AppConfig.singleton().getTransactionManager();
     var orderDao = new OrderDaoImpl();
     var ticketStockDao = new TicketStockDaoImpl();
 
@@ -29,7 +28,6 @@ public class ReadSideMain {
             var entity = new TicketStock();
             entity.setTickeId(event.ticketId);
             entity.setQuantity(event.quantity);
-
             transactionManager.required(() -> {
               ticketStockDao.insert(entity);
             });
